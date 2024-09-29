9a53751e51112e90a107bd12532b735cc0a2a70c
Benjamin M. <reallybaddeveloper@gmail.com>
2024-09-29 16:59:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+var devTime
+fetch(window.location.href + 'time').then((res) => {
+    console.log(res.text())
+	devTime = res.text()
+})
+
+setInterval(() => {
+	document.getElementById('time').innerText = new Date()
+		.toTimeString()
+		.split(' ')[0]
+}, 100)
+*/
